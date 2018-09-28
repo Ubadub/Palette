cff5257e7678a21b0a8924f6b8f3ecfff3f8a832
Abhinav Patil <apatil.cs@gmail.com>
2018-09-28 19:14:26 -0400

    Improved comments, removed unused imports, made some code more readable

--- palette.py
+++ palette.py
@@ -1,15 +1,15 @@
 from PIL import Image
-from heapq import nlargest
 from sklearn.cluster import MiniBatchKMeans
 import numpy as np
 
 PALETTE_WIDTH = 800
 PALETTE_HEIGHT = 200
-MAX_SIZE = 1000,1000
+MAX_SIZE = 500,500
 
 def make_palette(file):
 	"""
-	Given the path of a file, returns a list of lists representing the palette RGB values
+	Given the path of a file, returns a list of 8 lists, each of which describes
+	an RGB color as [R, G, B].
 	"""
 	im = Image.open(file)
 
@@ -17,9 +17,9 @@ def make_palette(file):
 	im.thumbnail(MAX_SIZE)
 
 	pixels = np.array([im.getpixel((x,y)) for x in range(0, im.size[0]) for y in range(0, im.size[1])])
-	clt = MiniBatchKMeans() #default is 8 clusters
+	clt = MiniBatchKMeans() # default is 8 clusters
 	clt.fit(pixels)
-	return [[round(i) for i in color] for color in clt.cluster_centers_]
+	return [[int(round(i)) for i in color] for color in clt.cluster_centers_]
 
 def main():
 	cluster_centers = make_palette(input("Enter name of file: "))
@@ -27,7 +27,7 @@ def main():
 	pixels = []
 	for i in range(8):
 		for j in range(PALETTE_WIDTH//8 * PALETTE_HEIGHT):
-			pixels.append(tuple([int(x) for x in cluster_centers[i]]))
+			pixels.append(tuple(cluster_centers[i]))
 
 	img = Image.new('RGB',(PALETTE_HEIGHT,PALETTE_WIDTH))
 	img.putdata(pixels) 
